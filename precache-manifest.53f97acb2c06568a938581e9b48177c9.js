a1ca82da35570ad2a5b343383b289d4a1590a365
Jacob Graczyk <jacobgraczyk@Jacobs-MacBook-Pro.local>
2020-05-12 10:02:07 -0400

    Updates

---
+++ precache-manifest.53f97acb2c06568a938581e9b48177c9.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be1ce1a9ae2ed62fe09724517f8eafc",
+    "url": "/hockey-faceoff/index.html"
+  },
+  {
+    "revision": "bf8438add5b95b4161fb",
+    "url": "/hockey-faceoff/static/css/main.b01fadd1.chunk.css"
+  },
+  {
+    "revision": "77f9de3260c1e197c616",
+    "url": "/hockey-faceoff/static/js/2.f6644cda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hockey-faceoff/static/js/2.f6644cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8438add5b95b4161fb",
+    "url": "/hockey-faceoff/static/js/main.544a216a.chunk.js"
+  },
+  {
+    "revision": "55a87528b26a20feac47",
+    "url": "/hockey-faceoff/static/js/runtime-main.d14b0202.js"
+  },
+  {
+    "revision": "d5deeeb5e5deaa9ad358baba6b06b50b",
+    "url": "/hockey-faceoff/static/media/avs-home.d5deeeb5.jpg"
+  },
+  {
+    "revision": "b478d8919b2a32dfa332638dee469122",
+    "url": "/hockey-faceoff/static/media/avs-road.b478d891.jpg"
+  },
+  {
+    "revision": "50e28d035d0cc687981460afe0c47b6d",
+    "url": "/hockey-faceoff/static/media/blues-home.50e28d03.jpg"
+  },
+  {
+    "revision": "6090b981dbfdb563394d5fc2b2212c63",
+    "url": "/hockey-faceoff/static/media/blues-road.6090b981.jpg"
+  },
+  {
+    "revision": "1759e5c842689c6a34401d59849e449d",
+    "url": "/hockey-faceoff/static/media/bruins-home.1759e5c8.jpg"
+  },
+  {
+    "revision": "a376f3f7869ba6b133cb43c7fa25339f",
+    "url": "/hockey-faceoff/static/media/bruins-road.a376f3f7.jpg"
+  },
+  {
+    "revision": "344d4f5ed25c021a5f0afa04bf92c5eb",
+    "url": "/hockey-faceoff/static/media/canes-home.344d4f5e.jpg"
+  },
+  {
+    "revision": "0ac82f5da883c4777721e5896652b306",
+    "url": "/hockey-faceoff/static/media/canes-road.0ac82f5d.jpg"
+  },
+  {
+    "revision": "e43b19e65c53e7ee1a3f239534e4e475",
+    "url": "/hockey-faceoff/static/media/canucks-home.e43b19e6.jpg"
+  },
+  {
+    "revision": "c33223db41b73e33985bc77e20669dd3",
+    "url": "/hockey-faceoff/static/media/canucks-road.c33223db.jpg"
+  },
+  {
+    "revision": "cf642661388567c0e8191023dbade293",
+    "url": "/hockey-faceoff/static/media/caps-home.cf642661.jpg"
+  },
+  {
+    "revision": "f919cf72a3d85a223c79b050ba17a2a3",
+    "url": "/hockey-faceoff/static/media/caps-road.f919cf72.jpg"
+  },
+  {
+    "revision": "587381f8c2bad8b59369a075f9fcf15d",
+    "url": "/hockey-faceoff/static/media/coyotes-home.587381f8.jpg"
+  },
+  {
+    "revision": "95ab5a0add87c05ad6464baf991a42cd",
+    "url": "/hockey-faceoff/static/media/coyotes-road.95ab5a0a.jpg"
+  },
+  {
+    "revision": "179a98174c3c46c2c55b1be79b55750d",
+    "url": "/hockey-faceoff/static/media/devils-home.179a9817.jpg"
+  },
+  {
+    "revision": "8b6a1085d2f5c984a4feaf66edcbc721",
+    "url": "/hockey-faceoff/static/media/devils-road.8b6a1085.jpg"
+  },
+  {
+    "revision": "58e879b0c06fc6535bf383822f08afa2",
+    "url": "/hockey-faceoff/static/media/ducks-home.58e879b0.jpg"
+  },
+  {
+    "revision": "beb25caf28e002402bb6c9ef64e4abe5",
+    "url": "/hockey-faceoff/static/media/ducks-road.beb25caf.jpg"
+  },
+  {
+    "revision": "54f3c95d2a11c3511a01b023fc166699",
+    "url": "/hockey-faceoff/static/media/flames-home.54f3c95d.jpg"
+  },
+  {
+    "revision": "1cb92d965f3be93c060651c1158e4e65",
+    "url": "/hockey-faceoff/static/media/flames-road.1cb92d96.jpg"
+  },
+  {
+    "revision": "9f64108bcd71e353ca64f8f5acbc8ca7",
+    "url": "/hockey-faceoff/static/media/flyers-home.9f64108b.jpg"
+  },
+  {
+    "revision": "59d505d5bcd5d88a059b41f4b613cb90",
+    "url": "/hockey-faceoff/static/media/flyers-road.59d505d5.jpg"
+  },
+  {
+    "revision": "a2ca6abfc953f6f34cfc4ab9892266ab",
+    "url": "/hockey-faceoff/static/media/habs-home.a2ca6abf.jpg"
+  },
+  {
+    "revision": "90073db8420fabe970b8dd5a8e39294d",
+    "url": "/hockey-faceoff/static/media/habs-road.90073db8.jpg"
+  },
+  {
+    "revision": "a18cbb92e4657dcabeb169ae5c971191",
+    "url": "/hockey-faceoff/static/media/hawks-home.a18cbb92.jpg"
+  },
+  {
+    "revision": "84ff411e656148b3a69e063e71ee5f66",
+    "url": "/hockey-faceoff/static/media/hawks-road.84ff411e.jpg"
+  },
+  {
+    "revision": "3a8012dfef59ed31d5d2276185d41af4",
+    "url": "/hockey-faceoff/static/media/islanders-home.3a8012df.jpg"
+  },
+  {
+    "revision": "ae27abeb7fb80e001e3ea25210d3f9f4",
+    "url": "/hockey-faceoff/static/media/islanders-road.ae27abeb.jpg"
+  },
+  {
+    "revision": "6d26aa81bf9b0deae167edac21ca9094",
+    "url": "/hockey-faceoff/static/media/jackets-home.6d26aa81.jpg"
+  },
+  {
+    "revision": "618d211d5fa3d3dc9225647b0bbebd42",
+    "url": "/hockey-faceoff/static/media/jackets-road.618d211d.jpg"
+  },
+  {
+    "revision": "6a8a3579b559697d4e47ab3b0cab7dca",
+    "url": "/hockey-faceoff/static/media/jets-home.6a8a3579.jpg"
+  },
+  {
+    "revision": "c443976e97c1e2b8862e40cc994b7101",
+    "url": "/hockey-faceoff/static/media/jets-road.c443976e.jpg"
+  },
+  {
+    "revision": "935318b1141be73dd7b8e61679d06aae",
+    "url": "/hockey-faceoff/static/media/kings-home.935318b1.jpg"
+  },
+  {
+    "revision": "e772cbb4988da8b80c79ecf7210481f7",
+    "url": "/hockey-faceoff/static/media/kings-road.e772cbb4.jpg"
+  },
+  {
+    "revision": "874bab0a2c3b21d095971fbf7ab3229d",
+    "url": "/hockey-faceoff/static/media/knights-home.874bab0a.jpg"
+  },
+  {
+    "revision": "62e62b82c69141d63db2c22e7cb6a109",
+    "url": "/hockey-faceoff/static/media/knights-road.62e62b82.jpg"
+  },
+  {
+    "revision": "1914c402c1a48eb6e796da64ce294561",
+    "url": "/hockey-faceoff/static/media/leafs-home.1914c402.jpg"
+  },
+  {
+    "revision": "8a57094c5758f8dc8e067a420837876c",
+    "url": "/hockey-faceoff/static/media/leafs-road.8a57094c.jpg"
+  },
+  {
+    "revision": "91faeb3976df4883b6f052a8451ebd3a",
+    "url": "/hockey-faceoff/static/media/lightning-home.91faeb39.jpg"
+  },
+  {
+    "revision": "7e9cf2117d9e965ac68f436ac10b2deb",
+    "url": "/hockey-faceoff/static/media/lightning-road.7e9cf211.jpg"
+  },
+  {
+    "revision": "f2c68567b7c2abc5307114cb018072dc",
+    "url": "/hockey-faceoff/static/media/logo.f2c68567.svg"
+  },
+  {
+    "revision": "0d99ce23e5f634a59e33682576c6b43f",
+    "url": "/hockey-faceoff/static/media/oilers-home.0d99ce23.jpg"
+  },
+  {
+    "revision": "62bcd460fe261e3cdd56a29e96397332",
+    "url": "/hockey-faceoff/static/media/oilers-road.62bcd460.jpg"
+  },
+  {
+    "revision": "36dcd40db1c5cfdc493477037cfcf619",
+    "url": "/hockey-faceoff/static/media/panthers-home.36dcd40d.jpg"
+  },
+  {
+    "revision": "f2761bbba020a12e3c9484ce6567da60",
+    "url": "/hockey-faceoff/static/media/panthers-road.f2761bbb.jpg"
+  },
+  {
+    "revision": "1c4c11a19b78f104c737e3da143c8541",
+    "url": "/hockey-faceoff/static/media/pens-home.1c4c11a1.jpg"
+  },
+  {
+    "revision": "a6d5d7c83806b8a9f427485130068507",
+    "url": "/hockey-faceoff/static/media/pens-road.a6d5d7c8.jpg"
+  },
+  {
+    "revision": "869e41a5d8fb0970c7414758af224ccc",
+    "url": "/hockey-faceoff/static/media/preds-home.869e41a5.jpg"
+  },
+  {
+    "revision": "c8306913c2b96677cad1827aa15808b2",
+    "url": "/hockey-faceoff/static/media/preds-road.c8306913.jpg"
+  },
+  {
+    "revision": "b10a737f6ab7bea4bd1f0ae223bfaee2",
+    "url": "/hockey-faceoff/static/media/rags-home.b10a737f.jpg"
+  },
+  {
+    "revision": "191e9f17e6d81cba2e6a047c6ff29eca",
+    "url": "/hockey-faceoff/static/media/rags-road.191e9f17.jpg"
+  },
+  {
+    "revision": "748c252f7fcd2fdc02e284f7c741abea",
+    "url": "/hockey-faceoff/static/media/sabres-home.748c252f.jpg"
+  },
+  {
+    "revision": "c95fb22d360d416978190609554aabf5",
+    "url": "/hockey-faceoff/static/media/sabres-road.c95fb22d.jpg"
+  },
+  {
+    "revision": "76d36d1ea84e443a82098056746529f7",
+    "url": "/hockey-faceoff/static/media/sens-home.76d36d1e.jpg"
+  },
+  {
+    "revision": "ec4c30dfe5db5b2f9c86057c1bb48a6e",
+    "url": "/hockey-faceoff/static/media/sens-road.ec4c30df.jpg"
+  },
+  {
+    "revision": "3f70942e0359de4b1bb634639bc11c73",
+    "url": "/hockey-faceoff/static/media/sharks-home.3f70942e.jpg"
+  },
+  {
+    "revision": "090eebf7db69a0020b4f69152c1f5cac",
+    "url": "/hockey-faceoff/static/media/sharks-road.090eebf7.jpg"
+  },
+  {
+    "revision": "c6a27a9a197e2b9183de47a0447efc58",
+    "url": "/hockey-faceoff/static/media/stars-home.c6a27a9a.jpg"
+  },
+  {
+    "revision": "f516d16b5701119e32486020cf6de881",
+    "url": "/hockey-faceoff/static/media/stars-road.f516d16b.jpg"
+  },
+  {
+    "revision": "3960329997b7966aa63f773e5e584dab",
+    "url": "/hockey-faceoff/static/media/wild-home.39603299.jpg"
+  },
+  {
+    "revision": "b4aef445c8f37013a455ae00a6d32285",
+    "url": "/hockey-faceoff/static/media/wild-road.b4aef445.jpg"
+  },
+  {
+    "revision": "9049705128f4c5d94457ea484cde6849",
+    "url": "/hockey-faceoff/static/media/wings-home.90497051.jpg"
+  },
+  {
+    "revision": "b035093e18684e1bfacc0205d535bf80",
+    "url": "/hockey-faceoff/static/media/wings-road.b035093e.jpg"
+  }
+]);
